1021c5547da91ba9eb5498e86da070c0885e886d
sanghyun97 <sanjungl97@naver.com>
2026-04-13 10:48:38 +0900

    파이어베이스 세팅 작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:yeouido_parking_flutter/firebase_options.dart';
 import 'package:yeouido_parking_flutter/utils/app_route/app_router.dart';
 import 'package:yeouido_parking_flutter/view/auth/auth_login.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
